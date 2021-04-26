70e7b0bc20603c0204f280feeb90b536a02d0fda
Srinivasarao Ippili <ippilisrinivas715@gmail.com>
2021-04-26 08:57:01 +0530

    Auto-generated commit

---
+++ precache-manifest.a8f641a188d37fa5a2ae73bf2b21ec3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7059420875801f24e470",
+    "url": "/ecomproj2/static/css/main.9709b156.chunk.css"
+  },
+  {
+    "revision": "7059420875801f24e470",
+    "url": "/ecomproj2/static/js/main.5234115d.chunk.js"
+  },
+  {
+    "revision": "2358d6f341f93ff00d84",
+    "url": "/ecomproj2/static/js/runtime~main.2e784eba.js"
+  },
+  {
+    "revision": "9254396549075ce598d9",
+    "url": "/ecomproj2/static/js/2.38fd7ff4.chunk.js"
+  },
+  {
+    "revision": "4290bc807f898ade8bc9a4296735ab10",
+    "url": "/ecomproj2/static/media/newLogo.4290bc80.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/ecomproj2/static/media/shopping-bag.09e6f5e7.svg"
+  },
+  {
+    "revision": "13e19a50718987167a30455477afb40a",
+    "url": "/ecomproj2/index.html"
+  }
+];
